f7c26a18027a3d708696bfe0ff6b10ae25370621
chamal pradeep <pradeephbac@gmail.com>
2019-04-20 02:10:11 +0800

    api changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  SUBCRIPRION_KEY: '98ed361cad61485cabe491e35a11c52b',
+  AZURE_API_URL:'https://australiaeast.api.cognitive.microsoft.com/vision/v2.0/analyze'
 };
