e90ff46f0bcb8b9d68d73582231edc26d9c409fa
chamal pradeep <pradeephbac@gmail.com>
2019-04-20 02:15:09 +0800

    change key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  SUBCRIPRION_KEY: '98ed361cad61485cabe491e35a11c52b',
+  SUBCRIPRION_KEY: '98ed361cad61485XXXXXXXXe35a11c52b',
   AZURE_API_URL:'https://australiaeast.api.cognitive.microsoft.com/vision/v2.0/analyze'
 };
 
